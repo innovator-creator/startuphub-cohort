2b96e92e9b46568aa2c37c29f7afa2423bebb5bb
JOHN MARK FORNAH <fornahinnovator@gmail.com>
2026-04-02 15:40:46 -0700

    Added About

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias iste cumque, asperiores voluptatibus quos distinctio laborum porro exercitationem dicta, vel tempora adipisci in dolorum. Et, ratione debitis? Aperiam, accusamus.</h1>
+      </div>    
+    );
+  }
+  
